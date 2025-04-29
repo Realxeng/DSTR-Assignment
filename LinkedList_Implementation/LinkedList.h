86f438328ff19d8ffdcc0fc58b2215c198a04826
StanleyWijayaa <stanleytan2018@gmail.com>
2025-04-29 16:31:15 +0800

    test

--- LinkedList_Implementation/LinkedList.h
+++ LinkedList_Implementation/LinkedList.h
@@ -6,11 +6,22 @@ class LinkedList
 {
     struct reviewsLL //Linked List for the reviews
     {
-        string ProductID, CustomerID, ReviewDesc;
+        string productID, customerID, reviewDesc;
         int Rate;
 
         reviewsLL* prev;
         reviewsLL* next;
     }*head, * tail;
+
+    struct transactionsLL
+    {
+        string customerID, product, category, date, paymentMethod;
+        float price;
+
+        transactionsLL* prev;
+        transactionsLL* next;
+    }*head, *tail;
+
+
 };
 
