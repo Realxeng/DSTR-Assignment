28894e918a5a5792cc19c65c2240978287cf5297
Realxeng <realxengpandelaki@gmail.com>
2025-05-02 21:46:26 +0800

    changed reviewsArray to use header file

---
+++ Array_Implementation/reviewsArray.cpp
@@ -0,0 +1,196 @@
+#include "reviewsArray.hpp"
+
+using namespace std;
+
+Reviews* list;
+reviewsArray::reviewsArray() {
+    list = NULL;
+    top = 0;
+}
+reviewsArray::reviewsArray(Reviews* list, int top) {
+    this->list = list;
+    this->top = top;
+}
+reviewsArray::reviewsArray(int size) {
+    list = new Reviews[size];
+    this->max = size;
+}
+reviewsArray::reviewsArray(ifstream& file) {
+    this->max = getMaxLine(file);
+    this->top = 0;
+    list = new Reviews[max];
+    insertFromFile(file);
+}
+reviewsArray::~reviewsArray() {
+    delete[] list;
+    list = nullptr;
+}
+
+int reviewsArray::getTop() { return top; }
+void reviewsArray::setTop(int top) { this->top = top; }
+
+int reviewsArray::getMaxLine(ifstream& file) {
+    int count = 0;
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            count++;
+        }
+    }
+    else {
+        cerr << "Unable to open file!";
+    }
+    file.clear(); // Clear EOF flag
+    file.seekg(0, ios::beg); // Move cursor to the beginning
+    return count;
+}
+
+void reviewsArray::insertToArray(Reviews data) {
+    list[top] = data;
+    top++;
+}
+
+void reviewsArray::insertFromFile(ifstream& file) {
+    if (file.is_open()) {
+        Reviews temp;
+        while (file.good()) {
+            string wholeline;
+            getline(file, wholeline, '\n');
+            istringstream iss(wholeline);
+            getline(iss, temp.pid, ',');
+            getline(iss, temp.cid, ',');
+            getline(iss, temp.rating, ',');
+            getline(iss, temp.review, '\n');
+            insertToArray(temp);
+        }
+    }
+    else {
+        cerr << "Unable to open file!";
+    }
+}
+
+void reviewsArray::insertFromFileRaw(ifstream& file) {
+    if (file.is_open()) {
+        Reviews temp;
+        string header;
+        getline(file, header);
+        while (file.good()) {
+            string wholeline;
+            getline(file, wholeline, '\n');
+            istringstream iss(wholeline);
+            getline(iss, temp.pid, ',');
+            getline(iss, temp.cid, ',');
+            getline(iss, temp.rating, ',');
+            getline(iss, temp.review, '\n');
+            insertToArray(temp);
+        }
+    }
+    else {
+        cerr << "Unable to open file!";
+    }
+}
+
+Reviews* reviewsArray::deleteAtIndex(Reviews* list, int index) {
+    if (index < 0 || index > top) {
+        cout << "No transaction found at index " << index << ". Top: " << top << endl;
+        return list;
+    }
+    for (int i = index; i < top - 1; i++) {
+        list[i] = list[i + 1];
+    }
+    top--;
+    return list;
+}
+
+void reviewsArray::deleteAtIndex(int index) {
+    if (index < 0 || index > top) {
+        cout << "Nothing to delete!";
+        return;
+    }
+    for (int i = index; i < top - 1; i++) {
+        list[i] = list[i + 1];
+    }
+    top--;
+}
+
+void reviewsArray::showAllReviews() {
+    for (int lines = 0; lines < max; lines++) {
+        cout << list[lines].pid << "|";
+        cout << list[lines].cid << "|";
+        cout << list[lines].rating << "|";
+        cout << list[lines].review << "|";
+        cout << endl;
+    }
+}
+
+reviewsArray reviewsArray::insertionSortCid()
+{
+    Reviews temp;
+    reviewsArray result = reviewsArray(top);
+    for (int i = 0; i < top; i++) {
+        temp.pid = list[i].pid;
+        temp.cid = list[i].cid;
+        temp.rating = list[i].rating;
+        temp.review = list[i].review;
+        if (i == 0) {
+            result.list[0] = temp;
+        }
+        else {
+            int j = 0;
+            while (j<i && temp.cid > result.list[j].cid) {
+                j++;
+                continue;
+            }
+            if (j == i) {
+                result.list[i] = temp;
+            }
+            else {
+                for (int k = i; k > j; k--) {
+                    result.list[k] = result.list[k - 1];
+                }
+                result.list[j] = temp;
+            }
+        }
+        //cout << " Record: " << top << " Progress: " << (float)top / 4128 * 100 << "%\n";
+    }
+    return result;
+}
+
+void reviewsArray::mergeSortByPID(int left = 0, int right = -1) {
+    if (right == -1) right = top - 1;
+    if (left >= right) return;
+
+    int mid = left + (right - left) / 2;
+
+    // Recursively divide
+    mergeSortByPID(left, mid);
+    mergeSortByPID(mid + 1, right);
+
+    // Temporary arrays
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    Reviews* L = new Reviews[n1];
+    Reviews* R = new Reviews[n2];
+
+    for (int i = 0; i < n1; i++) L[i] = list[left + i];
+    for (int i = 0; i < n2; i++) R[i] = list[mid + 1 + i];
+
+    // Merge step
+    cout << "left: " << left << endl;
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i].pid <= R[j].pid) list[k++] = L[i++];
+        else list[k++] = R[j++];
+    }
+    while (i < n1) {
+        cout << "Adding remaining L[" << i << "] = " << L[i].pid << " to list[" << k << "]" << endl;
+        list[k++] = L[i++];
+    }
+    while (j < n2) {
+        cout << "Adding remaining R[" << j << "] = " << R[j].pid << " to list[" << k << "]" << endl;
+        list[k++] = R[j++];
+    }
+
+    delete[] L;
+    delete[] R;
+}
--- Array_Implementation/reviewsArray.hpp
+++ Array_Implementation/reviewsArray.hpp
@@ -14,197 +14,21 @@ class reviewsArray
 {
     int top = 0, max = 0;
 public:
-    Reviews* list;
-    reviewsArray() {
-        list = NULL;
-        top = 0;
-    }
-    reviewsArray(Reviews* list, int top) {
-        this->list = list;
-        this->top = top;
-    }
-    reviewsArray(int size) {
-        list = new Reviews[size];
-		this->max = size;
-    }
-    reviewsArray(ifstream& file) {
-        this->max = getMaxLine(file);
-        this->top = 0;
-        list = new Reviews[max];
-        insertFromFile(file);
-    }
-	~reviewsArray() {
-		delete[] list;
-		list = nullptr;
-	}
-
-    int getTop() { return top; }
-    void setTop(int top) { this->top = top; }
-
-    int getMaxLine(ifstream& file) {
-        int count = 0;
-        if (file.is_open()) {
-            string line;
-            while (getline(file, line)) {
-                count++;
-            }
-        }
-        else {
-            cerr << "Unable to open file!";
-        }
-        file.clear(); // Clear EOF flag
-        file.seekg(0, ios::beg); // Move cursor to the beginning
-        return count;
-    }
-
-    void insertToArray(Reviews data) {
-        list[top] = data;
-        top++;
-    }
-
-    void insertFromFile(ifstream& file) {
-        if (file.is_open()) {
-            Reviews temp;
-            while (file.good()) {
-                string wholeline;
-                getline(file, wholeline, '\n');
-                istringstream iss(wholeline);
-                getline(iss, temp.pid, ',');
-                getline(iss, temp.cid, ',');
-                getline(iss, temp.rating, ',');
-                getline(iss, temp.review, '\n');
-                insertToArray(temp);
-            }
-        }
-        else {
-            cerr << "Unable to open file!";
-        }
-    }
-
-    void insertFromFileRaw(ifstream& file) {
-        if (file.is_open()) {
-            Reviews temp;
-			string header;
-			getline(file, header);
-            while (file.good()) {
-                string wholeline;
-                getline(file, wholeline, '\n');
-                istringstream iss(wholeline);
-                getline(iss, temp.pid, ',');
-                getline(iss, temp.cid, ',');
-                getline(iss, temp.rating, ',');
-                getline(iss, temp.review, '\n');
-                insertToArray(temp);
-            }
-        }
-        else {
-            cerr << "Unable to open file!";
-        }
-    }
-
-    Reviews* deleteAtIndex(Reviews* list, int index) {
-        if (index < 0 || index > top) {
-            cout << "No transaction found at index " << index << ". Top: " << top << endl;
-            return list;
-        }
-        for (int i = index; i < top - 1; i++) {
-            list[i] = list[i + 1];
-        }
-        top--;
-        return list;
-    }
-
-    void deleteAtIndex(int index) {
-        if (index < 0 || index > top) {
-            cout << "Nothing to delete!";
-            return;
-        }
-        for (int i = index; i < top - 1; i++) {
-            list[i] = list[i + 1];
-        }
-        top--;
-    }
-
-    void showAllReviews() {
-        for (int lines = 0; lines < max; lines++) {
-            cout << list[lines].pid << "|";
-            cout << list[lines].cid << "|";
-            cout << list[lines].rating << "|";
-            cout << list[lines].review << "|";
-            cout << endl;
-        }
-    }
-
-    reviewsArray insertionSortCid()
-    {
-        Reviews temp;
-        reviewsArray result = reviewsArray(top);
-        for (int i = 0; i < top; i++) {
-            temp.pid = list[i].pid;
-            temp.cid = list[i].cid;
-            temp.rating = list[i].rating;
-            temp.review = list[i].review;
-            if (i == 0) {
-                result.list[0] = temp;
-            }
-            else {
-                int j = 0;
-                while (j<i && temp.cid > result.list[j].cid) {
-                    j++;
-                    continue;
-                }
-                if (j == i) {
-                    result.list[i] = temp;
-                }
-                else {
-                    for (int k = i; k > j; k--) {
-                        result.list[k] = result.list[k - 1];
-                    }
-                    result.list[j] = temp;
-                }
-            }
-            //cout << " Record: " << top << " Progress: " << (float)top / 4128 * 100 << "%\n";
-        }
-        return result;
-    }
-
-    void mergeSortByPID(int left = 0, int right = -1) {
-        if (right == -1) right = top - 1;
-        if (left >= right) return;
-
-        int mid = left + (right - left) / 2;
-
-        // Recursively divide
-        mergeSortByPID(left, mid);
-        mergeSortByPID(mid + 1, right);
-
-        // Temporary arrays
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-        Reviews* L = new Reviews[n1];
-        Reviews* R = new Reviews[n2];
-
-        for (int i = 0; i < n1; i++) L[i] = list[left + i];
-        for (int i = 0; i < n2; i++) R[i] = list[mid + 1 + i];
-
-        // Merge step
-        cout << "left: "<< left<<endl;
-        int i = 0, j = 0, k = left;
-        while (i < n1 && j < n2) {
-            if (L[i].pid <= R[j].pid) list[k++] = L[i++];
-            else list[k++] = R[j++];
-        }
-        while (i < n1) {
-            cout << "Adding remaining L[" << i << "] = " << L[i].pid << " to list[" << k << "]" << endl;
-            list[k++] = L[i++];
-        }
-        while (j < n2) {
-            cout << "Adding remaining R[" << j << "] = " << R[j].pid << " to list[" << k << "]" << endl;
-            list[k++] = R[j++];
-        }
-
-        delete[] L;
-        delete[] R;
-    }
-
+	Reviews* list;
+	reviewsArray();
+	reviewsArray(Reviews* list, int top);
+	reviewsArray(int size);
+	reviewsArray(ifstream& file);
+	~reviewsArray();
+	int getTop();
+	void setTop(int top);
+	int getMaxLine(ifstream& file);
+	void insertToArray(Reviews data);
+	void insertFromFile(ifstream& file);
+	void insertFromFileRaw(ifstream& file);
+	Reviews* deleteAtIndex(Reviews* list, int index);
+	void deleteAtIndex(int index);
+	void showAllReviews();
+	reviewsArray insertionSortCid();
+	void mergeSortByPID(int left, int right);
 };
