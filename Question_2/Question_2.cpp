e6c7cbd8320457c9342c3546dbf1200b3c755825
Realxeng <realxengpandelaki@gmail.com>
2025-05-02 13:54:25 +0800

    Finished Question_2

--- Question_2/Question_2.cpp
+++ Question_2/Question_2.cpp
@@ -13,11 +13,16 @@ int main()
 {
 	ifstream tfile(transactionsFile);
 	transactionsArray ta = transactionsArray(tfile);
-	ta = ta.insertionSortCid();
-	ta = ta.bubbleSortDate();
-	ta.showAllTransactions();
-	//cout << "Total transactions: " << ta.getTop() << endl;
-	//cout << "Total transactions in Electronics: " << ta.linearSearchCategory(ta, "Electronics").getTop() << endl;
-	//cout << "Total transactions in Electronics using Credit Card: " << ta.linearSearchPayment(ta, "Credit Card").getTop() << endl;
+	//ta = ta.insertionSortCid();
+	//ta = ta.bubbleSortDate();
+	//ta.showAllTransactions();
+	transactionsArray electronicsTransactions = ta.linearSearchCategory("Electronics");
+	electronicsTransactions.showAllTransactions();
+	cout << "Total transactions: " << ta.getTop() << endl;
+	cout << "Total transactions in Electronics: " << electronicsTransactions.getTop() << endl;
+	transactionsArray creditCardTransactions = electronicsTransactions.linearSearchPayment("Credit Card");
+	cout << "Total transactions in Electronics using Credit Card: " << creditCardTransactions.getTop() << endl;
+	float percentage = (float)creditCardTransactions.getTop() / (float)electronicsTransactions.getTop() * 100;
+	cout << "Percentage of purchases in Electronics using Credit Card: " << percentage << "%" << endl;
 	return 0;
 }
