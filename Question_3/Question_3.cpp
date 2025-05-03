443dc35fa268a2a384b8ceb961c7ea779bdb13a5
Realxeng <realxengpandelaki@gmail.com>
2025-05-03 17:44:23 +0800

    minor changes on question 3

--- Question_3/Question_3.cpp
+++ Question_3/Question_3.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <chrono>
 #include "../Array_Implementation/wordsArray.hpp"
 
 using namespace std;
+using namespace std::chrono;
 
 const string reviewsFile = "../data/reviews_cleaned.csv";
 
@@ -9,8 +11,12 @@ int main()
 {
     ifstream rfile(reviewsFile);
     reviewsArray rarr = reviewsArray(rfile);
+    auto startarray = high_resolution_clock::now();
     reviewsArray reviewOneRating = rarr.linearSearchRating(1);
     wordsArray warr = wordsArray(reviewOneRating);
+    auto stoparray = high_resolution_clock::now();
+    auto durationarray = duration_cast<microseconds>(stoparray - startarray);
     warr.showMostUsedWords();
     cout << "\nThe most frequent word is: \"" << warr.showWord(1) << "\"" << endl;
+    cout << "Time taken to show the most frequent word with array and linear search is: " << durationarray.count() << " microseconds" << endl;
 }
