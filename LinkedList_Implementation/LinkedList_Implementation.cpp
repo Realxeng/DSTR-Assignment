3fa5f29ebbf95e8ee162edf33481194cd731904f
ReihanPramudito <reihan.dp2005@gmail.com>
2025-05-04 14:18:56 +0800

    Q4 COMPLETE

--- LinkedList_Implementation/LinkedList_Implementation.cpp
+++ LinkedList_Implementation/LinkedList_Implementation.cpp
@@ -7,16 +7,16 @@
 
 #include "ReviewsLinkedList.hpp"
 #include "ReviewsLinkedList.cpp"
-// #include "TransactionsLinkedList.hpp"
-// #include "TransactionsLinkedList.cpp"
+#include "TransactionsLinkedList.hpp"
+#include "TransactionsLinkedList.cpp"
 using namespace std;
 using namespace std::chrono;
 
 
 // Define Function in MAIN
 ReviewsLinkedList setUp_reviewLL();
-// void performSortAndSearch(transactionsLinkedList& transLL, bool useInsertionSort);
-// void performSearch(transactionsLinkedList& transLL);
+void performSortAndSearch(transactionsLinkedList& transLL, bool useInsertionSort);
+void performSearch(transactionsLinkedList& transLL);
 
 
 int main()
@@ -101,82 +101,82 @@ int main()
     }
     else if (choice == 2) 
     {
-        // transactionsLinkedList transLL = transLL.setUp_transactionLL();
+        transactionsLinkedList transLL = transLL.setUp_transactionLL();
 
-        // cout << "Transactions Linked List Menu:\n";
-        // cout << "1. Display Transaction Records\n";
-        // cout << "2. Sort by Date (using insertion sort)\n";
-        // cout << "3. Sort by Date (using bubble sort)\n";
+        cout << "Transactions Linked List Menu:\n";
+        cout << "1. Display Transaction Records\n";
+        cout << "2. Sort by Date (using insertion sort)\n";
+        cout << "3. Sort by Date (using bubble sort)\n";
 
-        // cin >> choice;
-        // cin.ignore();
+        cin >> choice;
+        cin.ignore();
 
-        // if (choice == 1)
-        // {
-        //     cout << "Total Records: " << transLL.getLLSize() << endl;
-        //     transLL.display();
-        // }
-        // else if (choice == 2) 
-        // {
-        //     performSortAndSearch(transLL, true);  //true = insertion sort
-        // }
-        // else if (choice == 3) 
-        // {
-        //     performSortAndSearch(transLL, false); //false = bubble sort
-        // }
+        if (choice == 1)
+        {
+            cout << "Total Records: " << transLL.getLLSize() << endl;
+            transLL.display();
+        }
+        else if (choice == 2) 
+        {
+            performSortAndSearch(transLL, true);  //true = insertion sort
+        }
+        else if (choice == 3) 
+        {
+            performSortAndSearch(transLL, false); //false = bubble sort
+        }
 
     }
     return 0;
 }
 
-// void performSearch(transactionsLinkedList& transLL) {
-//     int searchChoice;
-//     cout << "\nSearch by:\n1. Category\n2. Payment Method\nEnter choice: ";
-//     cin >> searchChoice;
+void performSearch(transactionsLinkedList& transLL) {
+    int searchChoice;
+    cout << "\nSearch by:\n1. Category\n2. Payment Method\nEnter choice: ";
+    cin >> searchChoice;
 
-//     cin.ignore(); // clear newline
-//     string searchTerm;
+    cin.ignore(); // clear newline
+    string searchTerm;
 
-//     if (searchChoice == 1) {
-//         cout << "Enter category: ";
-//         getline(cin, searchTerm);
+    if (searchChoice == 1) {
+        cout << "Enter category: ";
+        getline(cin, searchTerm);
 
-//         auto start = high_resolution_clock::now();
-//         transLL.searchByCategory(searchTerm);
-//         auto end = high_resolution_clock::now();
-//         cout << "Search completed in " << duration_cast<milliseconds>(end - start).count() << " ms.\n";
+        auto start = high_resolution_clock::now();
+        transLL.searchByCategory(searchTerm);
+        auto end = high_resolution_clock::now();
+        cout << "Search completed in " << duration_cast<milliseconds>(end - start).count() << " ms.\n";
 
-//     }
-//     else if (searchChoice == 2) {
-//         cout << "Enter payment method: ";
-//         getline(cin, searchTerm);
+    }
+    else if (searchChoice == 2) {
+        cout << "Enter payment method: ";
+        getline(cin, searchTerm);
 
-//         auto start = high_resolution_clock::now();
-//         transLL.searchByPaymentMethod(searchTerm);
-//         auto end = high_resolution_clock::now();
-//         cout << "Search completed in " << duration_cast<milliseconds>(end - start).count() << " ms.\n";
-//     }
-//     else {
-//         cout << "Invalid choice.\n";
-//     }
-// }
+        auto start = high_resolution_clock::now();
+        transLL.searchByPaymentMethod(searchTerm);
+        auto end = high_resolution_clock::now();
+        cout << "Search completed in " << duration_cast<milliseconds>(end - start).count() << " ms.\n";
+    }
+    else {
+        cout << "Invalid choice.\n";
+    }
+}
 
-// void performSortAndSearch(transactionsLinkedList& transLL, bool useInsertionSort) {
-//     auto start = high_resolution_clock::now();
+void performSortAndSearch(transactionsLinkedList& transLL, bool useInsertionSort) {
+    auto start = high_resolution_clock::now();
 
-//     if (useInsertionSort)
-//         transLL.SortByDate();
-//     else
-//         transLL.bubbleSortByDate();
+    if (useInsertionSort)
+        transLL.SortByDate();
+    else
+        transLL.bubbleSortByDate();
 
-//     auto end = high_resolution_clock::now();
-//     transLL.display();
-//     cout << (useInsertionSort ? "\nInsertion Sort" : "\nBubble Sort")
-//         << " completed in: "
-//         << duration_cast<milliseconds>(end - start).count() << " ms\n";
+    auto end = high_resolution_clock::now();
+    transLL.display();
+    cout << (useInsertionSort ? "\nInsertion Sort" : "\nBubble Sort")
+        << " completed in: "
+        << duration_cast<milliseconds>(end - start).count() << " ms\n";
 
-//     performSearch(transLL);
-// }
+    performSearch(transLL);
+}
 
 ReviewsLinkedList setUp_reviewLL(){
     // Reviews Linked List
--- Question_4/Question_4.cpp
+++ Question_4/Question_4.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
+#include <chrono>
+#include <fstream>
+#include <sstream>
 #include "../Array_Implementation/reviewsArray.hpp"
 #include "../Array_Implementation/wordsArray.hpp"
+#include "../LinkedList_Implementation/ReviewsLinkedList.hpp"
+#include "../LinkedList_Implementation/ReviewsLinkedList.cpp"
+
 
 using namespace std;
+using namespace std::chrono;
 
 const string reviewsFile = "../data/reviews_cleaned.csv";
+
+ReviewsLinkedList setUp_reviewLL();
 
 //Q: Which rating occurs most frequently for product PROD820 in the reviews dataset?
 
 int main() {
+
+    // Array Implementation
 	ifstream rfile(reviewsFile);
 	reviewsArray rarr = reviewsArray(rfile);
 	string pid;
@@ -19,4 +30,54 @@ int main() {
 	wordsArray wa = wordsArray(raProduct, 1);
 	wa.showMostFrequentRatings();
 	cout << "The most frequent rating for "<< raProduct.list[0].pid<< " is: " << wa.showWord(1);
+
+    // Linked List Implementation
+    ReviewsLinkedList reviewsLL = setUp_reviewLL();  // Set up the linked list with reviews
+    string productID;
+    cout << "Enter the Product ID your want to search: ";
+    getline(cin, productID);
+    auto start = high_resolution_clock::now();  // Start timing
+    reviewsLL.sortByRating(productID);  // Sort the linked list by rating with Merge Sort
+    auto end = high_resolution_clock::now();    // End timing
+    
+    cout << "\nThe most frequent rating for " << productID << " is: " << reviewsLL.rf_head->rating << endl;
+    cout << "Sorting of Reviews by Rating using Merge Sort completed in: "
+        << duration_cast<microseconds>(end - start).count() << " microseconds\n";
+}
+
+ReviewsLinkedList setUp_reviewLL(){
+    // Reviews Linked List
+    ReviewsLinkedList reviewsLL;
+
+    ifstream file("../data/reviews_cleaned.csv");
+    if (!file.is_open()) 
+    {
+        cerr << "Error opening file!" << endl;
+        return reviewsLL; // Return empty linked list
+    }
+
+    string line;
+
+    while (getline(file, line)) 
+    {
+        stringstream ss(line);
+        string productID, customerID, reviewDesc;
+        int rate;
+
+        getline(ss, productID, ',');
+        getline(ss, customerID, ',');
+        ss >> rate;
+        ss.ignore(); // Ignore the comma
+        getline(ss, reviewDesc);
+
+        // Insert into linked list
+        reviewsLL.insertNode_atEnd(productID, customerID, rate, reviewDesc);
+    }
+
+    file.close();
+    cout << "\nReviews linked list created and populated.\n";
+    cout << "Number of reviews: " << reviewsLL.getSize() << endl;
+
+    return reviewsLL;
+
 }
