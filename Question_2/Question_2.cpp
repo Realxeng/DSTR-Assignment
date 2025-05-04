225fe7fcf361d5b1a66893e9f3f0a0a6d1b1e913
Realxeng <realxengpandelaki@gmail.com>
2025-05-04 22:51:24 +0800

    Add time count for question 2

--- Question_2/Question_2.cpp
+++ Question_2/Question_2.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <chrono>
 #include "../Array_Implementation/transactionsArray.hpp"
 #include "../LinkedList_Implementation/TransactionsLinkedList.hpp"
 
 using namespace std;
+using namespace std::chrono;
 
 const string transactionsFile = "../data/transactions_cleaned.csv";
 
@@ -21,25 +23,32 @@ int main()
 		cin >> choice;
 		if (choice == 1) {
 			ifstream tfile(transactionsFile);
+			auto start = high_resolution_clock::now();
 			transactionsArray ta = transactionsArray(tfile);
 			transactionsArray electronicsTransactions = ta.linearSearchCategory("Electronics");
-			//electronicsTransactions.showAllTransactions();
 			cout << "Total transactions: " << ta.getTop() << endl;
 			cout << "Total transactions in Electronics: " << electronicsTransactions.getTop() << endl;
 			transactionsArray creditCardTransactions = electronicsTransactions.linearSearchPayment("Credit Card");
+			auto stop = high_resolution_clock::now();
+			auto duration = duration_cast<microseconds>(stop - start);
 			cout << "Total transactions in Electronics using Credit Card: " << creditCardTransactions.getTop() << endl;
 			float percentage = (float)creditCardTransactions.getTop() / (float)electronicsTransactions.getTop() * 100;
 			cout << "Percentage of purchases in Electronics using Credit Card: " << percentage << "%" << endl;
+			cout << "Time taken to do linear search for payment using array is: " << duration.count() << "microseconds" << endl;
 		}
 		else if (choice == 2) {
+			auto start = high_resolution_clock::now();
 			transactionsLinkedList tll = tll.setUp_transactionLL();
 			transactionsLinkedList tllcat = tll.returnByCategory("Electronics");
 			transactionsLinkedList tllcatpay = tllcat.returnByPaymentMethod("Credit Card");
+			auto stop = high_resolution_clock::now();
+			auto duration = duration_cast<microseconds>(stop - start);
 			cout << "Total transactions: " << tll.getLLSize() << endl;
 			cout << "Total transactions in Electronics: " << tllcat.getLLSize() << endl;
 			cout << "Total transactions in Electronics using Credit Card: " << tllcatpay.getLLSize() << endl;
 			float percentage = (float)tllcatpay.getLLSize() / (float)tllcat.getLLSize() * 100;
 			cout << "Percentage of purchases in Electronics using Credit Card: " << percentage << "%" << endl;
+			cout << "Time taken to do linear search for payment using linked list is: " << duration.count() << "microseconds" << endl;
 		}
 		else {
 			cout << "Exiting Program.." << endl;
